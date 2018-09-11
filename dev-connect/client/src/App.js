9e7a141013cc3bbc401938e6b8f20249cabcad2e
Prince K <k.prince1997@gmail.com>
2018-09-11 15:21:03 -0700

    Added more front end for profile components

--- dev-connect/client/src/App.js
+++ dev-connect/client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux-store';
 import jwt_decode from 'jwt-decode';
@@ -12,6 +12,7 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/common/PrivateRoute';
 import { SET_CURRENT_USER } from './actions/types';
 
 // Check for token
@@ -37,7 +38,9 @@ class App extends Component {
             <div className="container">
               <Route path="/login" component={Login} exact />
               <Route path="/register" component={Register} exact />
-              <Route path="/dashboard" component={Dashboard} exact />
+              <Switch>
+                <PrivateRoute path="/dashboard" component={Dashboard} exact />
+              </Switch>
             </div>
             <Footer />
           </div>
--- dev-connect/client/src/components/dashboard/Dashboard.js
+++ dev-connect/client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { getCurrentProfile } from '../../actions/profile-action';
+import Spinner from '../common/Spinner';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -9,17 +11,56 @@ class Dashboard extends Component {
   }
 
   render() {
-    return <div />;
+    const { user } = this.props.auth;
+
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if (loading) {
+      dashboardContent = <Spinner />;
+    } else {
+      if (profile !== null)
+        if (Object.keys(profile).length > 0) {
+          dashboardContent = <h2>Profile exists!</h2>;
+        } else {
+          // User is logged in, but has not created a profile yet
+          dashboardContent = (
+            <div>
+              <p className="lead text-muted">Welcome {user.name}</p>
+              <p> You have not created a profile yet! </p>
+              <Link to="/create-profile" className="btn btn-lg btn-info">
+                Create Profile
+              </Link>
+            </div>
+          );
+        }
+    }
+
+    return (
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
 Dashboard.propTypes = {
   profile: PropTypes.object.isRequired,
-  getCurrentProfile: PropTypes.func.isRequired
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  profile: state.profile
+  profile: state.profile,
+  auth: state.auth
 });
 
 export default connect(
