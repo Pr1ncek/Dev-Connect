e6a63b6dfaef9d9d6a6be0f9c7327afae0734c16
Prince K <k.prince1997@gmail.com>
2018-09-17 15:42:43 -0700

    added logic for adding posts

--- dev-connect/client/src/reducers/index.js
+++ dev-connect/client/src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import authReducer from './auth-reducer';
 import errorsReducer from './errors-reducer';
 import profileReducer from './profile-reducer';
+import postReducer from './post-reducer';
 
 export default combineReducers({
   auth: authReducer,
   profile: profileReducer,
-  errors: errorsReducer
+  errors: errorsReducer,
+  post: postReducer
 });
